7aea5f06b93b815056a42cec1d1e236476197e7b
JiaSing Lin <31025028+anthonyy41@users.noreply.github.com>
2021-11-28 13:04:30 +0800

    update basic call setting and add package

--- TodoApi_NET6_pratice/Program.cs
+++ TodoApi_NET6_pratice/Program.cs
@@ -1,18 +1,41 @@
-var builder = WebApplication.CreateBuilder(args);
+// import other package
+using Microsoft.EntityFrameworkCore;
 
-// Add services to the container.
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<TodoDb>(opt => opt.UseInMemoryDatabase("TodoList"));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 var app = builder.Build();
 
+// GET with default route
+app.MapGet("/", () => "load test");
+
+app.MapGet("/alltodoitem", async (TodoDb db) =>
+await db.Todos.ToListAsync());
+
+// Add services to the container.
+builder.Services.AddEndpointsApiExplorer();
+
+// 若出現錯誤時需要另外安裝套件 指令 : Install-Package Swashbuckle.AspNetCore -Version 6.2.3(版本)
+builder.Services.AddSwaggerGen();
+
+
 // Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
+
 
 var summaries = new[]
 {
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
+// example code
 app.MapGet("/weatherforecast", () =>
 {
     var forecast = Enumerable.Range(1, 5).Select(index =>
@@ -27,6 +50,26 @@ app.MapGet("/weatherforecast", () =>
 });
 
 app.Run();
+
+// create todo db class
+class Todo
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public bool IsCompleted { get; set; }
+    // public int Total { get; set; }
+}
+
+// create dbcontext with todo class
+class TodoDb : DbContext
+{
+    public TodoDb(DbContextOptions<TodoDb> options) : base(options)
+    {
+
+    }
+
+    public DbSet<Todo> Todos => Set<Todo>();
+}
 
 internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
 {
